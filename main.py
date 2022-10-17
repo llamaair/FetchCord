82125f1c85643ce675730c26203e2b3a18a0f3cc
FetchBotAdmin <83296140+llamaair@users.noreply.github.com>
2022-10-17 10:10:21 +0000

    Fixed bugs

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
-from predicts import PredictDigits
+def loaddiscord():
+  from discord import fetchdycord
 
-print("Modules loaded!")
+def loadpredicts():
+  from predicts import PredictDigits
+
+
+print("FetchCord waking up...")
+print("Loading modules")
+print("Loading discord cog")
+loaddiscord()
+print("Successfully loaded discord")
+print("Loading predicts cog")
+loadpredicts()
+print("Successfully loaded predicts")
+
+print("FetchCord is alive!")
+
+
+key="something"
+while key != "q":
+  loadings=str(input())
+  if loadings=="import imports":
+    print("Loading imports")
+    import imports
+    loadings=None
+  elif loadings=="import predicts":
+    print("Loading predicts")
+    loadpredicts()
+    loadings=None
+  elif loadings=="import discord":
+    print("Loading discord")
+    loaddiscord()
+    loadings=None
+  elif loadings=="quit":
+    print("Killing FetchCord")
+    quit()
