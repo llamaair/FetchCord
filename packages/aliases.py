2c08eb8d2c0e1036697d22c177b351a818f56e15
FetchBotAdmin <83296140+llamaair@users.noreply.github.com>
2022-10-18 07:54:27 +0000

    aliases

---
+++ packages/aliases.py
@@ -0,0 +1,13 @@
+#ALIASES FOR FETCHCORD
+from mainbot import command
+from mainbot import slash
+from mainbot import rolelock
+from mainbot import cooldown
+
+c = command
+
+sc = slash
+
+cool = cooldown
+
+rl = rolelock
